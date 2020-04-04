f816e10aa3b3cf3e593e530316e6da7c557b70ff
Siterizer <siterizer@gmail.com>
2020-04-04 11:01:58 +0200

    Added Reader classes

---
+++ src/main/java/library/readers/CSVReader.java
@@ -0,0 +1,13 @@
+package library.readers;
+
+import library.entities.Employee;
+
+import java.util.List;
+
+public class CSVReader implements EmployeeReader {
+
+    @Override
+    public List<Employee> readEmployee(String url) {
+        return null;
+    }
+}
---
+++ src/main/java/library/readers/EmployeeReader.java
@@ -0,0 +1,10 @@
+package library.readers;
+
+import library.entities.Employee;
+
+import java.util.List;
+
+public interface EmployeeReader {
+
+    public List<Employee> readEmployee(String url);
+}
