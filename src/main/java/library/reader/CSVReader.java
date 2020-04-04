12d2cb2118336f7180b081468352428994df98bf
Siterizer <siterizer@gmail.com>
2020-04-04 20:48:32 +0200

    Code refactoring in 'CSVReader' class

--- src/main/java/library/reader/CSVReader.java
+++ src/main/java/library/reader/CSVReader.java
@@ -15,31 +15,34 @@ public class CSVReader implements EmployeeReader {
 
     @Override
     public Employees readEmployee(String url) {
-        String line;
-        BufferedReader stream;
         List<List<String>> csvData = new ArrayList<>();
-        List<Employee> employees = new ArrayList<>();
-
         try {
-            stream = new BufferedReader(new FileReader(url));
-            stream.readLine();
+            BufferedReader stream = new BufferedReader(new FileReader(url));
+            stream.readLine();//get rid of attribute names
+            String line;
             while ((line = stream.readLine()) != null){
-                csvData.add(Arrays.asList(line.split(";")));
+                List<String> nextLine = Arrays.asList(line.split(";"));
+                for (int i = 0; i < nextLine.size(); i++)
+                {
+                    nextLine.set(i, nextLine.get(i)
+                            .replaceAll("[ \"]",""));
+                }
+                csvData.add(nextLine);
             }
         } catch (IOException e) {
             return null;
         }
+        List<Employee> employees = new ArrayList<>();
         for(List<String> values : csvData){
             employees.add(Employee.builder()
-                    .id(Long.parseLong(values.get(0).replaceAll("\\s+","").replace("\"", "")))
-                    .name(values.get(1).replaceAll("\\s+","").replace("\"", ""))
-                    .surname(values.get(2).replaceAll("\\s+","").replace("\"", ""))
-                    .job(values.get(3).replaceAll("\\s+","").replace("\"", ""))
-                    .salary(values.get(4).replaceAll("\\s+","").replace("\"", ""))
+                    .id(Long.parseLong(values.get(0)))
+                    .name(values.get(1))
+                    .surname(values.get(2))
+                    .job(values.get(3))
+                    .salary(values.get(4))
                     .build()
             );
         }
-
         return new Employees(employees);
     }
 }
